db2403191f5bf1abfe816e5cdcaf1281f20ab790
N. Suryakala <79616131+suryakala-1a@users.noreply.github.com>
2024-10-16 13:58:29 +0530

    Delete sample-potfolio-deployment-test directory

--- sample-potfolio-deployment-test/sampleportfolio.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("theme-toggle").addEventListener("click", function () {
-    document.body.classList.toggle("dark-theme");
-
-    if (document.body.classList.contains("dark-theme")) {
-        this.textContent = "Switch to Light Theme";
-    } else {
-        this.textContent = "Switch to Dark Theme";
-    }
-});
